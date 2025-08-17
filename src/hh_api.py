2845c2f3155fe969a6ff8e5d22629b3627432411
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-08-17 23:00:36 +0300

    fixed hh_api.py

--- src/hh_api.py
+++ src/hh_api.py
@@ -32,14 +32,14 @@ class HHApi(AbstractApi):
         """
         Инициализация API.
         """
-        self.__url = 'https://api.hh.ru/vacancies'
-        self.__params = {'page': page, 'per_page': 30}
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__params = {"page": page, "per_page": 30}
 
     def _connect(self, text: str):
         """
         Отправка запроса к API hh.ru.
         """
-        self.__params['text'] = text
+        self.__params["text"] = text
         response = requests.get(self.__url, self.__params)
         response.raise_for_status()
         return response
@@ -49,13 +49,13 @@ class HHApi(AbstractApi):
         Получение вакансий с hh.ru по ключевому слову.
         """
         all_vacancies = []
-        while self.__params['page'] < page:
-            vacancies = self._connect(text).json()['items']
+        while self.__params["page"] < page:
+            vacancies = self._connect(text).json()["items"]
             all_vacancies.extend(vacancies)
-            self.__params['page'] += 1
+            self.__params["page"] += 1
         return all_vacancies
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     hh = HHApi()
-    print(hh.get_vacancies('python', 1))
+    print(hh.get_vacancies("python", 1))
